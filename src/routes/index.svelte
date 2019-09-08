dc6afa0d6189cfcd42dbb7840e16e6f37f10ac32
romcar <rommel.carrasquilla@gmail.com>
2019-09-08 13:19:23 -0400

    Add luminance function for button-base mixin. Add pow. Add linearRGB functions.

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,42 +1,37 @@
-<style>
-	h1, figure, p {
-		text-align: center;
-		margin: 0 auto;
-	}
-
-	h1 {
-		font-size: 2.8em;
-		text-transform: uppercase;
-		font-weight: 700;
-		margin: 0 0 0.5em 0;
-	}
-
-	figure {
-		margin: 0 0 1em 0;
-	}
-
-	img {
-		width: 100%;
-		max-width: 400px;
-		margin: 0 0 1em 0;
-	}
-
-	p {
-		margin: 1em auto;
-	}
-
-	@media (min-width: 480px) {
-		h1 {
-			font-size: 4em;
-		}
-	}
-</style>
-
 <svelte:head>
 	<title>Sapper project template</title>
 </svelte:head>
 
 <h1>Great success!</h1>
+<h2>Great success!</h2>
+<h3>Great success!</h3>
+<h4>Great success!</h4>
+<h5>Great success!</h5>
+<h6>Great success!</h6>
+<p>Spicy jalapeno bacon ipsum dolor amet pork loin cupim bresaola alcatra drumstick rump kielbasa tail jowl ball tip andouille porchetta meatloaf turkey pork belly. Pork belly boudin ribeye swine chicken. Pork belly meatball pork chop shoulder ball tip andouille beef ribs. T-bone pork chop ham hock andouille, sausage shank pork belly doner ball tip biltong alcatra.
+<br/>
+Ham hock porchetta shank, shoulder tri-tip cupim pork frankfurter flank cow landjaeger buffalo turkey pork chop filet mignon. Rump ham hock ham andouille. Corned beef cupim t-bone, turkey ham fatback jowl. Ribeye rump andouille kevin. Landjaeger meatloaf tongue salami. Kevin burgdoggen ball tip filet mignon venison leberkas ham chicken.
+<br/>
+Short ribs pastrami corned beef shank venison tenderloin pork sirloin beef hamburger. Tri-tip tail sirloin chuck salami ground round drumstick leberkas pastrami tenderloin frankfurter hamburger. Boudin meatball cupim prosciutto beef pig. Biltong chicken ball tip swine short loin boudin shankle kielbasa shank t-bone. Bacon buffalo alcatra landjaeger shank, tenderloin tail cupim beef meatball chicken meatloaf prosciutto burgdoggen fatback.
+<br/>
+Leberkas pork filet mignon pork chop, swine t-bone rump turducken. Strip steak fatback ham corned beef brisket bresaola. Salami tenderloin porchetta brisket cupim beef. Pork belly buffalo sirloin filet mignon ham swine t-bone meatball spare ribs venison. Pork chop ball tip t-bone, tri-tip porchetta pancetta turkey venison kevin sirloin bresaola tail ham hock picanha pig. Swine meatball pig brisket, spare ribs turducken meatloaf boudin doner beef ribs cow cupim t-bone prosciutto ball tip.
+<br/>
+Jowl meatloaf swine, kielbasa leberkas biltong venison brisket pork loin fatback buffalo beef beef ribs turducken. Shankle burgdoggen sirloin, frankfurter ham hock pork chop capicola short loin pork chicken ground round pastrami tongue tenderloin jerky. Tenderloin t-bone tongue, shankle rump chicken porchetta pig kevin. Picanha pork loin filet mignon, jowl short loin alcatra boudin cupim bresaola t-bone meatloaf shank shoulder. Shoulder sirloin chicken t-bone porchetta shank ball tip frankfurter. Spare ribs burgdoggen porchetta tenderloin flank jowl.</p>
+
+<textarea name="text-area" cols="50" rows="10"></textarea>
+<button class="primary">BUTTON</button>
+<button class="secondary">BUTTON</button>
+
+<button class="primary error">BUTTON</button>
+<button class="primary warning">BUTTON</button>
+<button class="primary success">BUTTON</button>
+<button class="primary success disabled">BUTTON</button>
+
+<button class="secondary error">BUTTON</button>
+<button class="secondary warning">BUTTON</button>
+<button class="secondary success">BUTTON</button>
+<button class="secondary success disabled">BUTTON</button>
+
 
 <figure>
 	<img alt='Borat' src='great-success.png'>
@@ -44,3 +39,4 @@
 </figure>
 
 <p><strong>Try editing this file (src/routes/index.svelte) to test live reloading.</strong></p>
+<p><em>Try editing this file (src/routes/index.svelte) to test live reloading.</em></p>
